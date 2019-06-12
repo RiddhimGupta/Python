2299c8d4cfafd8cf36a6c65146dd09b94761bbeb
Riddhim Gupta <gupta.riddhim1@gmail.com>
2019-06-12 15:56:19 +0530

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -10,8 +10,10 @@ with sr.Microphone(device_index=9) as source:
     try:
         text = r.recognize_google(audio)
         print("You said : {}"+text)
+        url=[i for i in search(text,stop=10)]
+        print(url," \n")
     except:
         print("Sorry could not recognize your voice")
-web=input("Please enter topic to be searched: ")
-url=[i for i in search(web,stop=10)]
-print(url," \n")
+        web=input("Please enter topic to be searched: ")
+        url=[i for i in search(web,stop=10)]
+        print(url," \n")
