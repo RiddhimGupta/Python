ba6cd905495556fe10586b3f50a25cb3379d205d
Riddhim Gupta <gupta.riddhim1@gmail.com>
2019-06-12 15:34:33 +0530

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -3,16 +3,15 @@ import speech_recognition as sr
 from googlesearch import search
 import time
 r = sr.Recognizer()
-with sr.Microphone() as source:
+with sr.Microphone(device_index=9) as source:
     print("Speak Anything :")
-    audio = r.listen(source)
+    r.adjust_for_ambient_noise(source)
+    audio = r.record(source, duration=2.8)
     try:
         text = r.recognize_google(audio)
-        print("You said : {}".format(text))
-        url=[i for i in search(text,stop=10)]
-        print(url," \n")
+        print("You said : {}"+text)
     except:
         print("Sorry could not recognize your voice")
-        web=input("Please enter topic to be searched: ")
-        url=[i for i in search(web,stop=10)]
-        print(url," \n")
+web=input("Please enter topic to be searched: ")
+url=[i for i in search(web,stop=10)]
+print(url," \n")
